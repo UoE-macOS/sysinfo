88f8e20cfe90fc3c51b819a6911d8270fd06888d
Stew Wilson <stew.wilson@ed.ac.uk>
2016-07-19 12:55:32 +0100

    Initial Commit

---
+++ sysinfo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  sysinfo
+//
+//  Created by Stew Wilson on 19/07/2016.
+//  Copyright © 2016 University of Edinburgh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
