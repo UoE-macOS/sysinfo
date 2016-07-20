96f2d9bf9b86215530dc9f38f461351e8e838df5
Stew Wilson <stew.wilson@ed.ac.uk>
2016-07-20 11:06:08 +0100

    Initial commit of "menu-mode" app
    This version shows the hostname in a menu (along with "quit"); hostname appears clickable but doesn't actually do anything except closing the menu.

--- sysinfo/AppDelegate.swift
+++ sysinfo/AppDelegate.swift
@@ -14,46 +14,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
 
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
-    let popover = NSPopover()
     
-    var eventMonitor: EventMonitor?
-
+    
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         if let button = statusItem.button {
             button.image = NSImage(named: "StatusBarButtonImage")
-            button.action = #selector(AppDelegate.togglePopover(_:))
         }
         
-        popover.contentViewController = InfoViewController(nibName: "InfoViewController", bundle: nil)
+        var hostName: String!
+        //hostName = NSHost.currentHost().localizedName
+        hostName = "ENG-C02L30RADNCW"
+        let menu = NSMenu()
         
-        eventMonitor = EventMonitor(mask: [.LeftMouseDownMask, .RightMouseDownMask]) { [unowned self] event in
-            if self.popover.shown {
-                self.closePopover(event)
-            }
-        }
-        eventMonitor?.start()
+        menu.addItem(NSMenuItem(title: hostName, action: Selector("doNothing:"), keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separatorItem())
+        menu.addItem(NSMenuItem(title: "Quit Quotes", action: Selector("terminate:"), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
     }
     
-    func showPopover(sender: AnyObject?) {
-        if let button = statusItem.button {
-            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
-        }
-        eventMonitor?.start()
+    func doNothing(Sender: AnyObject) {
+        // Blank selector, so the menu item appears enabled.
     }
-    
-    func closePopover(sender: AnyObject?) {
-        popover.performClose(sender)
-        eventMonitor?.stop()
-    }
-    
-    func togglePopover(sender: AnyObject?) {
-        if popover.shown {
-            closePopover(sender)
-        } else {
-            showPopover(sender)
-        }
-    }
- 
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
